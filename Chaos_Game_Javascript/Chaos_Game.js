34ca0eee15eba30824fda8db64a93aece6c9e6e6
Dzmitry Paulenka <dmitri.pavlenko@gmail.com>
2018-04-23 00:41:55 +0300

    Warning and typos correction

--- Chaos_Game_Javascript/Chaos_Game.js
+++ Chaos_Game_Javascript/Chaos_Game.js
@@ -1,8 +1,8 @@
-var canvas = document.getElementById("chaos_game");
-var context  = canvas.getContext("2d");
+const canvas = document.getElementById('chaos_game');
+const context  = canvas.getContext('2d');
 
-var width  = window.innerWidth;
-var height = window.innerHeight;
+const width  = window.innerWidth;
+const height = window.innerHeight;
 
 //Set canvas width and height
 canvas.width  = width;
@@ -10,6 +10,7 @@ canvas.height = height;
 
 function wavelengthToRGB(wavelength)
 {   // Convert wavelength to RGB color. Approximately human eye vision.
+    let red, green, blue;  // RGB colors
     if((wavelength >= 400.0) && (wavelength <= 440.0)) {
         red = -1.0 * ((wavelength - 440.0) / 40.0);
         green = 0.0;
@@ -40,9 +41,9 @@ function wavelengthToRGB(wavelength)
         green = 0.0;
         blue = 0.0;
     }
-    return 'rgb(' + Math.floor(255*red) + ',' +
+    return 'rgb(' + Math.floor(255*red)   + ',' +
                     Math.floor(255*green) + ',' +
-                    Math.floor(255*blue) + ')';
+                    Math.floor(255*blue)  + ')';
 }
 
 function chaos_game(vertices, // number of vertices
@@ -51,45 +52,45 @@ function chaos_game(vertices, // number of vertices
                     height,   // screen height
                     inColor)  // color mode
 {   // Draw fractal itself
-    var w = width / 2.0;
-    var h = height / 2.0;
+    const w = width / 2.0;
+    const h = height / 2.0;
 
-    var rotOffset = Math.PI * (2.0/vertices - 0.5);
-    var polyhedron = [];
+    const rotOffset = Math.PI * (2.0/vertices - 0.5);
+    let   polyhedron = [];
 
-    for (var i = 0; i < vertices; ++i)
+    for (let i = 0; i < vertices; ++i)
     {
-        var cx = (w / 2) * Math.cos((2.0*Math.PI*i / vertices) + rotOffset) + w;
-        var cy = (h / 2) * Math.sin((2.0*Math.PI*i / vertices) + rotOffset) + h;
+        const cx = (w / 2) * Math.cos((2.0*Math.PI*i / vertices) + rotOffset) + w;
+        const cy = (h / 2) * Math.sin((2.0*Math.PI*i / vertices) + rotOffset) + h;
 
         polyhedron.push({x:cx, y:cy});
     }
 
-    var vertex = 0; // starting point in zero vertex
-    var rx = polyhedron[vertex].x;
-    var ry = polyhedron[vertex].y;
+    let vertex = 0; // starting point in zero vertex
+    let rx = polyhedron[vertex].x;
+    let ry = polyhedron[vertex].y;
 
     context.clearRect(0, 0, width, height); // Clear screen from previous drawing
     // Add text to the canvas
-    context.font = "16px Arial";
-    context.fillStyle = "black";
-    context.fillText("Use controls <Up>,<Down>,<Left>,<Right> or <Space>",10,20)
-    context.fillText("For color press <1> or <2>",10,40);
-    context.fillText("Vertices: " + vertices + "; Scale: " + scale.toFixed(3),10,60);
+    context.font = '16px Arial';
+    context.fillStyle = 'black';
+    context.fillText('Use controls <Up>,<Down>,<Left>,<Right> or <Space>', 10, 20);
+    context.fillText('For color press <1> or <2>', 10, 40);
+    context.fillText('Vertices: ' + vertices + '; Scale: ' + scale.toFixed(3), 10, 60);
 
-    var iterations = 100000;
-    for (i = 0; i < iterations; i++)
+    const iterations = 100000;
+    for (let i = 0; i < iterations; i++)
     {
         vertex = Math.floor(Math.random() * vertices); // randomly choose vertex
-        var dx = polyhedron[vertex].x - rx; // x-distance from vertex to point
-        var dy = polyhedron[vertex].y - ry; // y-distance from vertex to point
+        const dx = polyhedron[vertex].x - rx; // x-distance from vertex to point
+        const dy = polyhedron[vertex].y - ry; // y-distance from vertex to point
         rx += scale * dx; // new x-position of the point
         ry += scale * dy; // new y-position of the point
 
         if (inColor)
         {   // Set color to the image
-            var dist = Math.sqrt(dx * dx + dy * dy);
-            var wl = 400.0 + 300.0 * dist / h;
+            const dist = Math.sqrt(dx * dx + dy * dy);
+            const wl = 400.0 + 300.0 * dist / h;
             context.fillStyle = wavelengthToRGB(wl);
         }
 
@@ -97,17 +98,17 @@ function chaos_game(vertices, // number of vertices
     }
 }
 
-var upper_limit = 1.6;
-var lower_limit = 0.3;
-var max_vertices = 12;
-var min_vertices = 3;
-var vertices = min_vertices;
-var scale = lower_limit;
-var step = 0.005;
-var interval = 1; // milliseconds
-var inColor = true;
+const upper_limit = 1.6;
+const lower_limit = 0.3;
+const max_vertices = 12;
+const min_vertices = 3;
+const interval = 1; // repeat in milliseconds
+let vertices = min_vertices;
+let scale = lower_limit;
+let step = 0.005;
+let is_color = true;
 
-var id = setInterval(frame, interval); // repeat periodically
+let id = setInterval(frame, interval); // repeat periodically
 
 function frame()
 {   // Draw fractal with appropriate parameters
@@ -118,18 +119,18 @@ function frame()
     }
     
     if (scale > upper_limit || scale < lower_limit)
-    {   // Add one more vertex to polyhedron and reverce scale alteration
+    {   // Add one more vertex to polyhedron and reverse scale alteration
         vertices++;
         step = - step;
     }
     
     scale += step;
-    chaos_game(vertices, scale, width, height, inColor);
+    chaos_game(vertices, scale, width, height, is_color);
 }
 
-var is_moving = true;
+let is_moving = true;
 document.body.onkeydown = function(e) // Keyboard management
-{   // Execute after pressing spacebar
+{   // Execute after pressing space bar
     if (e.keyCode === 32)
     {
         switch (is_moving)
@@ -181,21 +182,20 @@ document.body.onkeydown = function(e) // Keyboard management
         if (scale < lower_limit) { scale = lower_limit; }
         if (scale > upper_limit) { scale = upper_limit; }
 
-        chaos_game(vertices, scale, width, height, inColor); // draw canvas
+        chaos_game(vertices, scale, width, height, is_color); // draw canvas
     }
 
     if (e.keyCode === 49 || e.keyCode === 50)
     {
         switch (e.keyCode)
         {
-            case 49: // "1" key
-                inColor = true;
+            case 49: // '1' key
+                is_color = true;
                 break;
-            case 50: // "2" key
-                inColor = false;
+            case 50: // '2' key
+                is_color = false;
                 break;
         }
-        chaos_game(vertices, scale, width, height, inColor); // draw canvas
+        chaos_game(vertices, scale, width, height, is_color); // draw canvas
     }
-}
-
+};
